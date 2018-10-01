38b4fadbafec6262108ca8461d5f8cea26697dc1
github username <juliannne@gmail.com>
2018-10-01 15:21:37 -0400

    Add form

---
+++ src/components/NewRoomForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class NewRoomForm extends React.Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input
+            text="text"
+            placeholder="NewRoomForm"
+          />
+        </form>
+      </div>
+    )
+  }
+}
