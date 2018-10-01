590c4299c90072b2c6d8e21a6d15d9f4eab77759
github username <juliannne@gmail.com>
2018-10-01 15:28:55 -0400

    Add constructor and handleChange

--- src/components/NewRoomForm.js
+++ src/components/NewRoomForm.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
 class NewRoomForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      roomName: ""
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      roomName: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <form>
           <input
+            onChange={this.handleChange}
             text="text"
             placeholder="NewRoomForm"
           />
+        <button type="submit"></button>
         </form>
       </div>
     )
   }
 }
+
+export default NewRoomForm
