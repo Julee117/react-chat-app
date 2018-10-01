b616adff0d52e2f77d0336e621645bf37067a624
github username <juliannne@gmail.com>
2018-10-01 11:12:10 -0400

    Import Message

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Message from './Message';
 
 class MessageList extends React.Component {
   render() {
     return (
       <div className="message-list">
-        Hello
+        {this.props.messages.map((message, index) => {
+          return (
+            <Message key={index} username={message.senderId} text={message.text}/>
+          )
+        })}
       </div>
     )
   }
