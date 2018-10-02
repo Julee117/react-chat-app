780292b46fdd2f7131331b4ee0a0a1909586a9fd
github username <juliannne@gmail.com>
2018-10-02 10:49:33 -0400

    Add conditional

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -17,12 +17,19 @@ class MessageList extends React.Component {
   }
 
   render() {
+    if (!this.props.roomId) {
+      return (
+        <div className="message-list">
+          <div className="join-room">
+            Join a room!
+          </div>
+        </div>
+      )
+    }
     return (
       <div className="message-list">
         {this.props.messages.map((message, index) => {
-          return (
-            <Message key={index} username={message.senderId} text={message.text}/>
-          )
+          return <Message key={message.id} username={message.senderId} text={message.text}/>
         })}
       </div>
     )
