97c235a4225f5e4d500b9b524d3afc3d0dbac4a3
github username <juliannne@gmail.com>
2018-10-02 11:33:24 -0400

    Add ul tag

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -5,19 +5,21 @@ class Rooms extends React.Component {
     const orderedRooms = [...this.props.rooms].sort((a, b) => a.id - b.id);
     return (
       <div className="rooms-list">
-        <h3>Your rooms:</h3>
-        {orderedRooms.map(room => {
-          const active = this.props.roomId === room.id ? "active" : "";
-          return (
-            <li key={room.id} className={"room " + active}>
-              <a
-                onClick={() => {this.props.subscribeToRoom(room.id)}}
-                href="#">
-                # {room.name}
-              </a>
-            </li>
-          )
-        })}
+        <ul>
+          <h3>Your rooms:</h3>
+          {orderedRooms.map(room => {
+            const active = this.props.roomId === room.id ? "active" : "";
+            return (
+              <li key={room.id} className={"room " + active}>
+                <a
+                  onClick={() => {this.props.subscribeToRoom(room.id)}}
+                  href="#">
+                  # {room.name}
+                </a>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     )
   }
